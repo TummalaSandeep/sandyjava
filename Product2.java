371aeadf9c2ae0c63f32df2132988908545cdac9
TummalaSandeep <sandeep23.t@gmail.com>
2016-07-28 05:54:11 -0700

    Add files via upload

---
+++ Product2.java
@@ -0,0 +1,36 @@
+package com.niit.ShopingCart;
+
+public class Product2 {
+Private int id;
+private string name;
+
+public product(int id,string name);
+{
+	this.id=id;
+	this.name=name;
+	this.price=price;
+}
+public int price;
+public int getId();
+{
+	return id;
+}
+public void set Id(int id)
+{
+	this.id=id;
+	
+}
+public void set
+	public string getName() {
+	return name;
+}
+public void setName(string name) {
+	this.name = name;
+}
+public int getPrice() {
+	return price;
+}
+public void setPrice(int price) {
+	this.price = price;
+}
+}
